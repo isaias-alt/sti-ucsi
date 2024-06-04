7cbf18b240ffb136a790dd8113409aefb4ec82dc
isaias-at <cascolucasisaias@gmail.com>
2024-06-04 16:59:22 -0400

    add speaker data

---
+++ src/data/speakers.ts
@@ -0,0 +1,67 @@
+interface Speakers {
+  id: string
+  name: string
+  title: string
+  description: string
+  instagram: string
+  linkedin: string
+}
+
+export const SPEAKERS: Speakers[] = [
+	{
+		id: 'lucasdalto',
+		name: 'Lucas Dalto',
+		title: 'Navegando la Neblina: Inteligencia Artificial y la Incertidumbre Laboral en el Desarrollo de Software',
+		description: 'Descubre cómo la inteligencia artificial está transformando el desarrollo de software y cómo enfrentar los desafíos de la incertidumbre laboral en este campo.',
+		instagram: 'soydalto',
+		linkedin: 'soy-dalto'
+	},
+	{
+		id: 'kokidure',
+		name: 'Koki Dure',
+		title: 'Graduado en Informática: ¿Y Ahora Qué? Estrategias y Recomendaciones para el Inicio de tu Carrera',
+		description: 'Obtén consejos prácticos y estrategias clave para dar tus primeros pasos en la carrera de Informática después de graduarte, desde la búsqueda de empleo hasta el desarrollo profesional.',
+		instagram: 'kokidure',
+		linkedin: 'kokidure'
+	},
+	{
+		id: 'carlitosvargas',
+		name: 'Carlitos Vargas',
+		title: 'Flutter: La Opción que Tendrías que Considerar para Dedicarte al Desarrollo',
+		description: 'Explora por qué Flutter es una opción destacada para aquellos interesados en el desarrollo de aplicaciones móviles y cómo puede impulsar tu carrera en este campo.',
+		instagram: 'vargascarlitos',
+		linkedin: 'kalitodev'
+	},
+	{
+		id: 'alejandracanclini',
+		name: 'Alejandra Canclini',
+		title: 'Tecnología y Ambiente: Experiencias Prácticas de Utilización de Sistemas de Información Geográfica para Abordar Problemas Socioambientales',
+		description: 'Conoce casos reales donde la tecnología, especialmente los Sistemas de Información Geográfica, se ha utilizado para abordar problemas socioambientales, destacando la importancia de la tecnología en la conservación del medio ambiente.',
+		instagram: 'alecanclini',
+		linkedin: 'alejandracanclini1'
+	},
+	{
+		id: 'noheliarios',
+		name: 'Nohelia Rios',
+		title: 'Proyectos Tecnológicos Implementados dentro de la Agenda Digital Paraguay: Realidad y Desafíos',
+		description: 'Explora los proyectos tecnológicos más relevantes dentro de la Agenda Digital de Paraguay, analizando su impacto actual y los desafíos que enfrentan en su implementación.',
+		instagram: 'nohelia_rios_garcia',
+		linkedin: 'nohelia-rios-garcia-41594165'
+	},
+	{
+		id: 'benortega',
+		name: 'Ben Ortega',
+		title: 'La Informática: La Clave para una Agricultura y Ganadería Sostenible',
+		description: 'Descubre cómo la informática y las tecnologías digitales están revolucionando el sector agrícola y ganadero, promoviendo prácticas más sostenibles y eficientes.',
+		instagram: 'bentorres33',
+		linkedin: 'benito-ortega-torres-ab3555242'
+	},
+	{
+		id: 'simonbenitez',
+		name: 'Simon Benitez',
+		title: 'Inteligencia Artificial: ¿Héroe o Villano?',
+		description: 'Analiza de manera crítica el impacto de la inteligencia artificial en nuestra sociedad, discutiendo tanto sus beneficios como los desafíos éticos y sociales que plantea.',
+		instagram: 'simonandresbenitez',
+		linkedin: 'simonbenitezrodas'
+	},
+]
