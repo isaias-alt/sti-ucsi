72587a6c995a89535be287f5d10e72479a380fa1
isaias-at <cascolucasisaias@gmail.com>
2024-06-04 20:16:36 -0400

    modularize

---
+++ src/data/schedule.ts
@@ -0,0 +1,81 @@
+interface Schedule {
+  id: string
+  time: string
+  title: string
+  speaker: string
+}
+
+export const SCHEDULE = [
+  {
+		time: '07:30',
+		title: 'Acreditaciones',
+		speaker: 'Open Doors',
+	},
+  {
+    time: '08:05',
+    title: 'Apertura',
+    speaker: ' Mgter. Esperanza Del Puerto'
+  },
+  {
+    id: 'lucasdalto',
+    time: '08:20',
+    title: 'Navegando la Neblina: Inteligencia Artificial y la Incertidumbre Laboral en el Desarrollo de Software',
+    speaker: 'Lucas Dalto'
+  },
+  {
+    id: 'noheliarios',
+    time: '09:20',
+    title: 'Proyectos Tecnológicos Implementados dentro de la Agenda Digital Paraguay: Realidad y Desafíos',
+    speaker: 'Nohelia Rios'
+  },
+  {
+    time: '10:00',
+    title: 'Media mañana',
+    speaker: 'Cocido, Café, Chipas y más',
+  },
+  {
+    id: 'carlitosvargas',
+    time: '10:20',
+    title: 'Flutter: La Opción que Tendrías que Considerar para Dedicarte al Desarrollo',
+    speaker: 'Carlitos Vargas'
+  },
+  {
+    id: 'alejandracanclini',
+    time: '11:00',
+    title: 'Tecnología y Ambiente: Experiencias Prácticas de Utilización de Sistemas de Información Geográfica para Abordar Problemas Socioambientales',
+    speaker: 'Alejandra Canclini'
+  },
+  {
+    time: '11:40',
+    title: 'Almuerzo',
+    speaker: 'Bocaditos y Gaseosas',
+  },
+  {
+    id: 'benortega',
+    time: '12:40',
+    title: 'La Informática: La Clave para una Agricultura y Ganadería Sostenible',
+    speaker: 'Ben Ortega'
+  },
+  {
+    id: 'simonbenitez',
+    time: '13:20',
+    title: 'Inteligencia Artificial: \n ¿Héroe o Villano?',
+    speaker: 'Simon Benitez'
+  },
+  {
+    id: 'kokidure',
+    time: '14:10',
+    title: 'Graduado en Informática: ¿Y Ahora Qué? Estrategias y Recomendaciones para el Inicio de tu Carrera',
+    speaker: 'Koki Dure'
+  },
+  {
+    time: '15:10',
+    title: 'Cierre ',
+    speaker: 'Entrega de Certificados',
+  },
+  {
+    time: '15:30',
+    title: 'Networking',
+    speaker: 'Hotel Perla Negra',
+  }
+]
